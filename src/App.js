2a3992e4dc9605d71f76f1b9c7f31d117ad934fd
aadodo <32916700+aadodo@users.noreply.github.com>
2017-10-26 07:30:55 -0200

    atualização do backend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
-import CadastroForm from './Formulario.jsx';
+import CadastroForm from './formulario/Formulario.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Prova de Vida</h1>
           <h3> Cadastro de segurado</h3><br/>
         </header>                
